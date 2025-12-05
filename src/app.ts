715102e3b5abf7ab258c40a615224cdf886e1bf0
Nícolas Lemes <nicolastloliveira@gmail.com>
2025-12-05 12:50:42 -0300

    third class

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { createCourseRoute } from './routes/create-course.ts'
 import { getCourseByIdRoute } from './routes/get-course-by-id.ts'
 import { getCoursesRoute } from './routes/get-courses.ts'
 import scalarAPIReference from '@scalar/fastify-api-reference'
+import { loginRoute } from './routes/login.ts'
 
 const server = fastify({
   logger: {
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV === 'development') {
   server.register(fastifySwagger, {
     openapi: {
       info: {
-        title: 'Desafio Node.js',
+        title: 'First API',
         version: '1.0.0',
       }
     },
@@ -40,5 +41,6 @@ server.setSerializerCompiler(serializerCompiler)
 server.register(createCourseRoute)
 server.register(getCourseByIdRoute)
 server.register(getCoursesRoute)
+server.register(loginRoute)
 
 export { server }
